8da92bbde5d8b00ee202877110f2f0bbc20e5435
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-08-01 18:19:03 -0500

    Creating and improving the Discover Screen

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Discover Screen'),
-      ),
+    //* These are the two ways to access the provider
+
+    final discoverProvider = context.watch<DiscoverProvider>();
+    // final discoverProvider2 = Provider.of<DiscoverProvider>(context);
+
+    // final discoverProvider = context.read<DiscoverProvider>();
+    // final discoverProvider2 = Provider.of<DiscoverProvider>(context, listen: false);
+
+    return Scaffold(
+      body: discoverProvider.initialLoading
+          ? const Center(
+              child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ))
+          : VideoScrollableView(
+              videos: discoverProvider.videos,
+            ),
     );
   }
 }
