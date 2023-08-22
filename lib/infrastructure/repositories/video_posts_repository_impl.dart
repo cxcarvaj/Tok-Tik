ac10c611a2ca58fd472adedd28df6573f8ae3a3f
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-08-21 21:41:15 -0500

    feat(toktik_app): Implementation of datasources and repositories

---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+  final VideoPostDataSource videosDatasource;
+
+  VideoPostsRepositoryImpl({
+    required this.videosDatasource,
+  });
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideoByPage(int page) {
+    return videosDatasource.getTrendingVideoByPage(page);
+  }
+}
